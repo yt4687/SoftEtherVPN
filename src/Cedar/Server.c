4e1635414258d7a5244b1cd0155921f77563cb4b
yt4687 <35659282+yt4687@users.noreply.github.com>
2025-02-05 11:08:14 +0900

    Update Server.c

--- src/Cedar/Server.c
+++ src/Cedar/Server.c
@@ -10762,7 +10762,7 @@ bool SiIsEnterpriseFunctionsRestrictedOnOpenSource(CEDAR *c)
 
 	if (StrCmpi(region, "JP") == 0 || StrCmpi(region, "CN") == 0)
 	{
-		ret = true;
+		ret = false;
 	}
 
 	return ret;
